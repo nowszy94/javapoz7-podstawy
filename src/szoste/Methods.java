3cc7ecccf6bbca8a2e62027c5170e8ac24fd1ff9
Szymon Nowak <nowszy94@gmail.com>
2018-02-28 21:12:08 +0100

    methods

--- src/szoste/Methods.java
+++ src/szoste/Methods.java
@@ -1,5 +1,7 @@
 package szoste;
 
+import java.util.Arrays;
+
 public class Methods {
     //silnia (factorial) -> long
     //zliczanie liczb podzielnych przez 2, ale nie podzielnych przez 3
@@ -9,13 +11,16 @@ public class Methods {
     //split -> 123;abc;dd -> ["123", "abc", "dd"]
     //         dd.aa.cc -> ["dd", "aa", "cc"]
     //fibonacci
-    //sprawdzanie nawiasow w stringu
+    //odwracanie stringa test -> tset, abcde -> edcba
+    //sprawdzanie nawiasow w stringu ")("
+    //suma cyfr w stringu (wywloanie jakich metod?) -> "1234"
     //metoda do porownania 2 stringow ktore sa liczbami
+    //
     //metoda wyswietlajaca sume liczb pierwszych w danej liczbie (1,2,3,5,7,11) np.
     //              liczba 29 = 11 + 11 + 7
     //              liczba 31 = 11 + 11 + 7 + 2
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7,8,9};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         System.out.println("Factorial of 6 = " + factorial(6));
         System.out.println("Divisible by 2 and not by 3 = " + countDivisibleBy2AndNotBy3(array));
         System.out.println("Switched text = " + switchChars("ala ma kota", 'a', 'b'));
@@ -24,15 +29,159 @@ public class Methods {
         System.out.println("\"anna\" is palindrome = " + isPalindrome("anna"));
         System.out.println("\"1Kajak1\" is palindrome = " + isPalindrome("1Kajak1"));
         System.out.println("\"abcdefgh\" is palindrome = " + isPalindrome("abcdefgh"));
+        System.out.println("switched \"AbCd\" = " + switchCase("AbCd"));
+        System.out.println("a;b;c 0(-1) = " + getNthIndexOf(';', "a;b;c", 0));
+        System.out.println("a;b;c 1(1) = " + getNthIndexOf(';', "a;b;c", 1));
+        System.out.println("a;b;c 2(3) = " + getNthIndexOf(';', "a;b;c", 2));
+        System.out.println("a;b;c 3(5) = " + getNthIndexOf(';', "a;b;c", 3));
+        System.out.println("\"Ala ma kota\" split = " + Arrays.toString(split("Ala ma kota", ' ')));
+        System.out.println("\"ab;cd;ef\" split = " + Arrays.toString(split("ab;cd;ef", ';')));
+        System.out.println("\"ab;;cd;ef\" split = " + Arrays.toString(split("ab;;cd;ef", ';')));
+        System.out.println("\"ab;;cd;ef\" split = " + Arrays.toString(split("ab;;cd;ef;", ';')));
+        System.out.println("\"Ala ma kota\" split2 = " + Arrays.toString(split2("Ala ma kota", ' ')));
+        System.out.println("\"ab;cd;ef\" split2 = " + Arrays.toString(split2("ab;cd;ef", ';')));
+        System.out.println("\"ab;;cd;ef\" split2 = " + Arrays.toString(split2("ab;;cd;ef", ';')));
+        System.out.println("\"ab;;cd;ef\" split2 = " + Arrays.toString(split2("ab;;cd;ef;", ';')));
+        System.out.println("fibonacci of 0 = " + fibonacciNumber(0));
+        System.out.println("fibonacci of 1 = " + fibonacciNumber(1));
+        System.out.println("fibonacci of 5 = " + fibonacciNumber(5));
+        System.out.println("fibonacci of 10 = " + fibonacciNumber(10));
+        System.out.println("fibonacci of 15 = " + fibonacciNumber(15));
+        System.out.println("fibonacci array (5) = " + Arrays.toString(fibonacciArray(5)));
+        System.out.println("fibonacci array (7) = " + Arrays.toString(fibonacciArray(7)));
+        System.out.println("fibonacci array (10) = " + Arrays.toString(fibonacciArray(10)));
+        System.out.println("reverse of \"abcdef\" = " +reverse("abcdef"));
+        System.out.println("reverse of \"abcdefg\" = " +reverse("abcdefg"));
+        System.out.println("\"(a+b)\" is valid = " + isValidStatement("(a+b)"));
+        System.out.println("\"(a+b))\" is valid = " + isValidStatement("(a+b))"));
+        System.out.println("\")(a+b))\" is valid = " + isValidStatement(")(a+b))"));
+        System.out.println("\")(a+b)(\" is valid = " + isValidStatement(")(a+b)("));
+    }
+
+    public static boolean isValidStatement(String statetment) {
+        char[] array = statetment.toCharArray();
+        int counter = 0;
+        int i = 0;
+        while (i < array.length && counter >= 0) {
+            if (array[i] == '(') {
+                counter++;
+            } else if (array[i] == ')') {
+                counter--;
+            }
+            i++;
+        }
+        return counter == 0;
+    }
+
+    public static void swap() {
+        int a = 2;
+        int b = 5;
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    public static String reverse(String text) {
+        char[] array = text.toCharArray();
+        for (int i = 0; i < array.length / 2; i++) {
+            char tmp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = tmp;
+        }
+        return String.valueOf(array);
+    }
+
+    public static int[] fibonacciArray(int toIndex) {
+        int[] array = new int[toIndex];
+        array[0] = 0;
+        array[1] = 1;
+        for (int i = 2; i < array.length; i++) {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+        return array;
+    }
+
+    public static int fibonacciNumber(int n) {
+        if (n <= 1) return n;
+        int first = 0;
+        int second = 1;
+        for (int i = 2; i <= n; i++) {
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        return second;
+    }
+
+    public static String[] split2(String message, char c) {
+        int size = Warmup.lettersCounter(message, c) + 1;
+        String[] resultArray = new String[size];
+        int start = 0;
+        int end;
+        for (int i = 0; i < resultArray.length; i++) {
+            String textToPut = "";
+            end = getNthIndexOf(c, message, (i + 1));
+            for (int j = start; j < end; j++) {
+                textToPut += message.charAt(j);
+            }
+            resultArray[i] = textToPut;
+            start = end + 1;
+        }
+        return resultArray;
     }
 
     //TODO
-    //    public static String[] split(String message, char c) {
-//        String[] array = new String[Warmup.lettersCounter(message, c)];
-//        int start = 0;
-////        int end = getNthIndexOf(c, message, 0);
-//        return array;
-//    }
+    public static String[] split(String message, char c) {
+        int size = Warmup.lettersCounter(message, c) + 1;
+        String[] resultArray = new String[size];
+        String textToPut = "";
+        int j = 0;
+        for (int i = 0; i < message.length(); i++) {
+            if (message.charAt(i) != c) {
+                textToPut += message.charAt(i);
+                if (i == message.length() - 1) {
+                    resultArray[j] = textToPut;
+                }
+            } else {
+                resultArray[j] = textToPut;
+                j++;
+                textToPut = "";
+            }
+        }
+        return resultArray;
+    }
+
+    public static int getNthIndexOf(char c, String text, int index) {
+        char[] chars = text.toCharArray();
+        int i = -1;
+        while (i < chars.length && index > 0) {
+            i++;
+            if (i < chars.length && c == chars[i]) {
+                index--;
+            }
+        }
+        return i;
+    }
+
+    public static String switchCase(String text) {
+        char[] chars = text.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = switchCase(chars[i]);
+        }
+        return String.valueOf(chars);
+    }
+
+    public static char switchCase(char c) {
+//        return (c >= 'a' && c <= 'z') ? (char)(c - 32) :
+//                ((c >= 'A' && c <= 'Z') ? (char)(c + 32) : c);
+        char switchedChar = c;
+        if (c >= 'a' && c <= 'z') {
+            switchedChar -= 32;
+        } else if (c >= 'A' && c <= 'Z') {
+            switchedChar += 32;
+        }
+        return switchedChar;
+    }
 
     public static boolean isPalindrome(String text) {
         char[] chars = text.toCharArray();
