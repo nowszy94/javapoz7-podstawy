31e0375dca36516ef01dac4615d4422ad93468c4
Szymon Nowak <nowszy94@gmai.com>
2018-03-13 21:04:15 +0100

    collections exercises

--- src/collections/CollectionsExercises.java
+++ src/collections/CollectionsExercises.java
@@ -1,5 +1,9 @@
 package collections;
 
+import oop.User;
+import oop.bookstore.Author;
+import oop.bookstore.Book;
+
 import java.util.*;
 
 public class CollectionsExercises {
@@ -28,6 +32,47 @@ public class CollectionsExercises {
         //utworzyc liste liczb (random) oraz wyswietlic liczby podzielne przez 2
         //utworzyc liste liczb (random) oraz wyswietlic tylko elementy na parzystych indeksach
         //na podstawie LinkedList, utworzyc kolejke fifo -> osobna klasa MyFifoQueue
+    }
+
+    public static void bookstore() {
+        Map<Author, List<Book>> bookstore = new HashMap<>();
+        bookstore.put(new Author("Adam Mickiewicz"),
+                Arrays.asList(
+                        new Book("Dziady 3", 1800),
+                        new Book("Dziady 4", 1802))
+        );
+        bookstore.put(new Author("Henryk Sienkiewicz"),
+                Arrays.asList(new Book("Quo Vadix", 1900),
+                        new Book("Krzyzacy", 1902)));
+
+        Set<Map.Entry<Author, List<Book>>> bookstoreSet = bookstore.entrySet();
+        for (Map.Entry<Author, List<Book>> authorListEntry : bookstoreSet) {
+            List<Book> books = authorListEntry.getValue();
+            for (Book book : books) {
+                if (book.getReleaseYear() > 1801) {
+                    System.out.println(book);
+                }
+            }
+        }
+    }
+
+    //utworzyc mape Pesel(String) -> User
+    public static void usersDatabase() {
+        Map<String, User> usersDatabase = new HashMap<>();
+        usersDatabase.put("123456789", new User("Szymon", "Nowak"));
+        usersDatabase.put("56789543", new User("Jan", "Kowalski"));
+        usersDatabase.put("asdf939232", new User("Karolina", "Wisniewska"));
+        Set<Map.Entry<String, User>> entries = usersDatabase.entrySet();
+        Iterator<Map.Entry<String, User>> iterator = entries.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, User> next = iterator.next();
+            System.out.println(next.getKey() + ": " + next.getValue());
+        }
+
+        for (Map.Entry<String, User> next : entries) {
+            System.out.println(next.getKey() + ": " + next.getValue());
+
+        }
     }
 
     public static void uniqueNumbersFromUser() {
--- src/oop/bookstore/Author.java
+++ src/oop/bookstore/Author.java
@@ -1,5 +1,7 @@
 package oop.bookstore;
 
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String birthDate;
@@ -27,6 +29,22 @@ public class Author {
 
     public String getLanguage() {
         return language;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) &&
+                Objects.equals(birthDate, author.birthDate) &&
+                Objects.equals(language, author.language);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, birthDate, language);
     }
 
     @Override
